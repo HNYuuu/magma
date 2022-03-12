45564affb748a85a51f57158a5343745d3a48d41
PisonJay <PisonJay@outlook.com>
2022-03-12 20:30:19 +0800

    updated libtiff/build.sh for wasm

--- targets/libtiff/build.sh
+++ targets/libtiff/build.sh
@@ -20,13 +20,13 @@ mkdir -p "$WORK/lib" "$WORK/include"
 
 cd "$TARGET/repo"
 ./autogen.sh
-./configure --disable-shared --prefix="$WORK"
-make -j$(nproc) clean
-make -j$(nproc)
-make install
+emconfigure ./configure --disable-shared --prefix="$WORK"
+emmake make -j$(nproc) clean
+emmake make -j$(nproc)
+emmake make install
 
-cp "$WORK/bin/tiffcp" "$OUT/"
-$CXX $CXXFLAGS -std=c++11 -I$WORK/include \
-    contrib/oss-fuzz/tiff_read_rgba_fuzzer.cc -o $OUT/tiff_read_rgba_fuzzer \
-    $WORK/lib/libtiffxx.a $WORK/lib/libtiff.a -lz -ljpeg -Wl,-Bstatic -llzma -Wl,-Bdynamic \
+cp "$TARGET/repo/tools/tiffcp.wasm" "$OUT/"
+emcc -g -std=c++11 -I$WORK/include \
+    contrib/oss-fuzz/tiff_read_rgba_fuzzer.cc -o $OUT/tiff_read_rgba_fuzzer.html \
+    $WORK/lib/libtiffxx.a $WORK/lib/libtiff.a -lz \
     $LDFLAGS $LIBS
