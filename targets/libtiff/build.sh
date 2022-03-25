9e708b533dfe15129dff873474df733a313f27fd
PisonJay <PisonJay@outlook.com>
2022-03-25 18:48:26 +0800

    remove tiffcp

--- targets/libtiff/build.sh
+++ targets/libtiff/build.sh
@@ -25,7 +25,6 @@ emmake make -j$(nproc) clean
 emmake make -j$(nproc)
 emmake make install
 
-cp "$TARGET/repo/tools/tiffcp.wasm" "$OUT/"
 emcc -g -std=c++11 -I$WORK/include \
     contrib/oss-fuzz/tiff_read_rgba_fuzzer.cc -o $OUT/tiff_read_rgba_fuzzer.html \
     $WORK/lib/libtiffxx.a $WORK/lib/libtiff.a -lz \
